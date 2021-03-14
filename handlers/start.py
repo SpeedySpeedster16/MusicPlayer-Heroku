43867008d0347c0a046c1369ca5f5ee839e05171
SpeedySpeedster16 <78364856+SpeedySpeedster16@users.noreply.github.com>
2021-03-14 11:59:42 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,6 +4,38 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(
     filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
+
+I am Music Player, I will play music in your groups. Add me and @Firefrost_sudo in group. Dont forget to make admin.
+
+Use the buttons below to know more about me.""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔰Add me🔰", url="https://t.me/Vchatprobot?startgroup=true"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🔥Group🔥", url="https://t.me/Mysterybotschat"
+                    ),
+                    InlineKeyboardButton(
+                        "♻️Channel♻️", url="https://t.me/mysterybots"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(
+    filters.command("start")
     & filters.group
     & ~ filters.edited
 )
